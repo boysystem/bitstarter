50379e890ae1032535530da2f3dc7b69ecd01e56
Ubuntu <ubuntu@ip-172-31-36-206.us-west-2.compute.internal>
2013-07-28 12:13:32 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,12 +1,31 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+ar express = require('express');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var fs = require('fs');
+
+var buffer = "";
+
+fs.readFile('index.html', function (err, data) {
+
+if (err) throw err;
+
+buffer = data.toString();
+
 });
 
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+
+response.send(buffer);
+
+});
+
+
+
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+
+console.log("Listening on " + port);
+
 });
